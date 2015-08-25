169c05ff3f86442f2e607c0d080ead258a93a105
mikaZJ <zhuziyunjing@163com>
2015-08-25 11:48:53 +0800

    my first project on github

---
+++ js/test0.js
@@ -0,0 +1,31 @@
+
+//ÃüÃû¿Õ¼ä
+window.onload=function(){
+    mv.app.toTip();
+};
+var mv = {};
+
+mv.tools ={};
+
+mv.ui={};
+
+mv.ui.textChange=function(obj,str){
+    obj.onfocus=function(){
+        if(this.value==str){
+            this.value="";
+        }
+        obj.onblur=function(){
+            if(this.value==""){
+                this.value=str;
+            }
+    }
+};
+mv.app={};
+
+mv.app.toTip=function(){
+    var oText1=document.getElementById("text1");
+    var oText2=document.getElementById("text2");
+
+    mv.ui.textChange(oText1,"Search Website");
+    mv.ui.textChange(oText2,"Search Website");
+}
